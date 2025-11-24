74eb571940a4204a3315037d7ac9089decabb4c5
Brielle <brielle_hoff@mines.edu>
2025-11-24 15:17:39 -0700

    Refactor mk-csv.sh
    mk-csv.sh now handles subdirectories (probably unnecessary) and includes the
    `minecraft:` prefix in the name column

--- src/mk-csv.sh
+++ src/mk-csv.sh
@@ -4,12 +4,15 @@
 # ./src/mk-csv.sh ./mc-assets/data/minecraft/recipe/ > ./mc-data/recipes.csv
 # ./src/mk-csv.sh ./mc-assets/data/minecraft/tags/item/ > ./mc-data/tags.csv
 
+# cd to directory of json files
 cd "$1"
 
-for file in *.json; do
-    printf "${file%.json}"
-    printf ","
+# loop over json files
+find . -name '*.json' -printf '%P\0' |
+while IFS= read -r -d $'\0' file; do
     printf '"'
-    sed 's/"/""/g' < "$file" | tr -d $'\r\n'
+    printf 'minecraft:%s' "${file%.json}" | sed 's/"/""/g'
+    printf '","'
+    sed 's/"/""/g' < "./$file" | tr -d $'\r\n'
     printf $'"\r\n'
 done
