39d2748c2ec529acf3414eff05f27b03dda5ae05
Brielle <brielle_hoff@mines.edu>
2025-11-24 15:30:16 -0700

    add sql code to generate item/tag join table

--- src/load.sql
+++ src/load.sql
@@ -1,6 +1,7 @@
 SET search_path TO minecraft_recipes;
 
 
+  -- load raw accumulated json csv files
 DROP TABLE IF EXISTS recipes_json;
 CREATE TABLE recipes_json (name TEXT, json JSON);
 \copy recipes_json from './mc-data/recipes.csv' (FORMAT CSV);
@@ -10,6 +11,7 @@ CREATE TABLE tags_json(name TEXT, json JSON);
 \copy tags_json from './mc-data/tags.csv' (FORMAT CSV);
 
 
+  -- extract categories
 DROP TABLE IF EXISTS category CASCADE;
 CREATE TABLE category (id TEXT PRIMARY KEY);
 INSERT INTO category (
@@ -19,6 +21,7 @@ INSERT INTO category (
     ) WHERE id IS NOT NULL
 );
 
+  -- extract groups
 DROP TABLE IF EXISTS recipe_group CASCADE;
 CREATE TABLE recipe_group (id TEXT PRIMARY KEY);
 INSERT INTO recipe_group (
@@ -28,8 +31,38 @@ INSERT INTO recipe_group (
     ) WHERE id IS NOT NULL
 );
 
+  -- extract tags
 DROP TABLE IF EXISTS tag CASCADE;
 CREATE TABLE tag (id TEXT PRIMARY KEY);
 INSERT INTO tag (
     SELECT name FROM tags_json
+);
+
+
+  -- create item/tag join table
+DROP TABLE IF EXISTS item_tag CASCADE;
+CREATE TABLE item_tag (
+    item TEXT NOT NULL,
+    tag  TEXT NOT NULL REFERENCES tag(id),
+    PRIMARY KEY (item, tag)
+);
+
+WITH RECURSIVE unexpanded_tags AS (
+    SELECT name AS tag, json_table.*
+    FROM tags_json, JSON_TABLE (json, '$.values[*]' COLUMNS (
+        item TEXT PATH '$'
+    )) AS json_table
+),
+expanded_tags AS (
+    SELECT *
+    FROM unexpanded_tags
+    UNION ALL
+    SELECT base.tag, sub.item
+    FROM expanded_tags base
+    JOIN unexpanded_tags sub ON base.item = ('#' || sub.tag)
+)
+INSERT INTO item_tag (
+    SELECT DISTINCT item, tag
+    FROM expanded_tags
+    WHERE item NOT LIKE '#%'
 );
